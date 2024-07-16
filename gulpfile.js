7c4e2b629a08b91ca72be409277e8a3c728a3513
Dave Barreto <dave@orangetreesolutions.com>
2024-07-16 14:22:58 -0700

    package fixes to theme-kit

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
-var gulp  = require('gulp'),
-    sass = require('gulp-sass'),
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
     sourcemaps = require('gulp-sourcemaps'),
     cleanCss = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    postcss      = require('gulp-postcss'),
+    postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer');
 
 function buildCss() {
     return gulp.src(['scss/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer({ browsers: [
-            'Chrome >= 35',
-            'Firefox >= 38',
-            'Edge >= 12',
-            'Explorer >= 10',
-            'iOS >= 8',
-            'Safari >= 8',
-            'Android 2.3',
-            'Android >= 4',
-            'Opera >= 12']})]))
+        .pipe(postcss([autoprefixer({
+            browsers: [
+                'Chrome >= 35',
+                'Firefox >= 38',
+                'Edge >= 12',
+                'Explorer >= 10',
+                'iOS >= 8',
+                'Safari >= 8',
+                'Android 2.3',
+                'Android >= 4',
+                'Opera >= 12']
+        })]))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css/'))
         .pipe(cleanCss())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css/'))
 }
 
